5ad783a23f1151a23f0cc64e15dd4bedc2c30981
Marco Antonio Lopez-Cruz <malctony@hotmail.com>
2024-02-02 11:39:09 -0500

    Add files via upload

--- misc/code_doc/kronecker_bm1.R
+++ misc/code_doc/kronecker_bm1.R
@@ -4,17 +4,16 @@ library(fastmatrix)
 library(ggplot2)
 
 # 1. Simulating small matrices
-m = 40; n = 75
-p = 50; q = 40
+m = 50; n = 60
+p = 40; q = 50
 A <- matrix(rnorm(m*n), ncol=n)
 B <- matrix(rnorm(p*q), ncol=q)
 
-gc()
 res1 <- microbenchmark(
          'kronecker\n(base)'            = kronecker(A,B),
          'kronecker.prod\n(fastmatrix)' = kronecker.prod(A,B),
          'Kronecker\n(tensorEVD)'       = Kronecker(A,B),
-       times = 20)
+       times = 30)
 
 tt1 <- paste0("'Kronecker('*A[",m,"*'x'*",n,"]*', '*B[",p,"*'x'*",q,"]*')'==K[",m*p,"*'x'*",n*q,"]")
 
@@ -24,24 +23,21 @@ p = 80; q = 100
 A <- matrix(rnorm(m*n), ncol=n)
 B <- matrix(rnorm(p*q), ncol=q)
 
-gc()
 res2 <- microbenchmark(
          'kronecker\n(base)'            = kronecker(A,B),
          'kronecker.prod\n(fastmatrix)' = kronecker.prod(A,B),
          'Kronecker\n(tensorEVD)'       = Kronecker(A,B),
-      times = 20)
+      times = 30)
 
 tt2 <- paste0("'Kronecker('*A[",m,"*'x'*",n,"]*', '*B[",p,"*'x'*",q,"]*')'==K[",m*p,"*'x'*",n*q,"]")
 
 # 3. Making a barplot
 res <- rbind(data.frame(group=tt1, expr=res1$expr, time=res1$time/1E9),
              data.frame(group=tt2, expr=res2$expr, time=res2$time/1E9))
-dat <- aggregate(time~expr+group, data=res, FUN=mean)
-dat$sd <- aggregate(time~expr+group, data=res, FUN=sd)$time
+dat <- aggregate(time~expr+group, data=res, FUN=median)
 
 pp <- ggplot(dat, aes(expr,time)) + theme_bw() +
-      geom_bar(stat="identity", fill='lightyellow3', width=0.65) +
-      geom_errorbar(aes(ymin=time-sd, ymax=time+sd), width=0.2) +
+      geom_bar(stat="identity", fill='lightyellow3', width=0.6) +
       labs(x=NULL, y="Time (seconds)") +
       facet_wrap(~group, scales="free", labeller=label_parsed)
 print(pp)
--- misc/code_doc/kronecker_bm2.R
+++ misc/code_doc/kronecker_bm2.R
@@ -2,7 +2,6 @@ library(microbenchmark)
 library(tensorEVD)
 library(ggplot2)
 
-
 # 1. Simulating A and B matrices
 m = 100; n = 50
 p = 50; q = 100
@@ -11,36 +10,34 @@ B <- matrix(rnorm(p*q), ncol=q)
 
 dm <- c(nrow(A)*nrow(B), ncol(A)*ncol(B))   # dimension of the Kronecker
 
-# 2. Subsetting a matrix with 20% of rows/columns
+# 2. Subsetting an small matrix with 20% of rows/columns
 rows1 <- sample(seq(dm[1]), 0.20*dm[1])
 cols1 <- sample(seq(dm[2]), 0.20*dm[2])
 
-# 3. Subsetting a matrix with 200% of rows/columns
+# 3. Subsetting a large matrix with 200% of rows/columns
 rows2 <- sample(seq(dm[1]), 2*dm[1], replace=TRUE)
 cols2 <- sample(seq(dm[2]), 2*dm[2], replace=TRUE)
 
-gc()
 res <- microbenchmark(
        'Kronecker(A,B,rows,cols)_Small submatrix'  = Kronecker(A,B,rows=rows1,cols=cols1),
        'Kronecker(A,B)[rows,cols]_Small submatrix' = Kronecker(A,B)[rows1,cols1],
        'Kronecker(A,B,rows,cols)_Large submatrix'  = Kronecker(A,B,rows=rows2,cols=cols2),
        'Kronecker(A,B)[rows,cols]_Large submatrix' = Kronecker(A,B)[rows2,cols2],
-     times = 20)
+     times = 30)
 
 # 4. Making a barplot
 res <- data.frame(expr=res$expr, time=res$time/1E9)
 res <- data.frame(res, do.call(rbind,strsplit(as.character(res$expr),"_")))
-dat <- aggregate(time~X1+X2, data=res, FUN=mean)
-dat$sd <- aggregate(time~X1+X2, data=res, FUN=sd)$time
-dat$X2 <- gsub("Small",paste0(length(rows1),"x",length(cols1)),dat$X2)
-dat$X2 <- gsub("Large",paste0(length(rows2),"x",length(cols2)),dat$X2)
+dat <- aggregate(time~X1+X2, data=res, FUN=median)
+dat$X2 <- paste0(dat$X2,": ",ifelse(dat$X2=="Small submatrix",
+                                    paste0(length(rows1),"x",length(cols1)),
+                                    paste0(length(rows2),"x",length(cols2))))
 
 title <- paste0("'Subsetting of Kronecker('*A[",m,"*'x'*",n,"]*', '*B[",p,"*'x'*",
                 q,"]*')'==K[",m*p,"*'x'*",n*q,"]")
 
 pp <- ggplot(dat, aes(X1,time)) + theme_bw() +
       geom_bar(stat="identity", fill='lightyellow3', width=0.6) +
-      geom_errorbar(aes(ymin=time-sd, ymax=time+sd), width=0.1) +
       labs(x=NULL, y="Time (seconds)", title=parse(text=title)) +
       facet_wrap(~X2, scales="free") +
       theme(plot.title=element_text(hjust=0.5))
