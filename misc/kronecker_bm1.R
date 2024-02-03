b23149cdeed93cbb121ca8968ef9115993e730e8
Marco Antonio Lopez-Cruz <malctony@hotmail.com>
2024-02-03 14:09:20 -0500

    Add files via upload

---
+++ misc/kronecker_bm1.R
@@ -0,0 +1,43 @@
+library(microbenchmark)
+library(tensorEVD)
+library(fastmatrix)
+library(ggplot2)
+
+# 1. Simulating a small Kronecker product
+m = 50; n = 60
+p = 40; q = 50
+A <- matrix(rnorm(m*n), ncol=n)
+B <- matrix(rnorm(p*q), ncol=q)
+
+res1 <- microbenchmark(
+         'kronecker\n(base)'            = kronecker(A,B),
+         'kronecker.prod\n(fastmatrix)' = kronecker.prod(A,B),
+         'Kronecker\n(tensorEVD)'       = Kronecker(A,B),
+       times = 30)
+
+tt1 <- paste0("'Kronecker('*A[",m,"*'x'*",n,"]*', '*B[",p,"*'x'*",q,"]*')'==K[",m*p,"*'x'*",n*q,"]")
+
+# 2. Simulating a large Kronecker product
+m = 100; n = 120
+p = 80; q = 100
+A <- matrix(rnorm(m*n), ncol=n)
+B <- matrix(rnorm(p*q), ncol=q)
+
+res2 <- microbenchmark(
+         'kronecker\n(base)'            = kronecker(A,B),
+         'kronecker.prod\n(fastmatrix)' = kronecker.prod(A,B),
+         'Kronecker\n(tensorEVD)'       = Kronecker(A,B),
+      times = 30)
+
+tt2 <- paste0("'Kronecker('*A[",m,"*'x'*",n,"]*', '*B[",p,"*'x'*",q,"]*')'==K[",m*p,"*'x'*",n*q,"]")
+
+# 3. Making a barplot
+res <- rbind(data.frame(group=tt1, expr=res1$expr, time=res1$time/1E9),
+             data.frame(group=tt2, expr=res2$expr, time=res2$time/1E9))
+dat <- aggregate(time~expr+group, data=res, FUN=median)
+
+pp <- ggplot(dat, aes(expr,time)) + theme_bw() +
+      geom_bar(stat="identity", fill='lightyellow3', width=0.6) +
+      labs(x=NULL, y="Time (seconds)") +
+      facet_wrap(~group, scales="free", labeller=label_parsed)
+print(pp)
---
+++ misc/kronecker_bm2.R
@@ -0,0 +1,46 @@
+library(microbenchmark)
+library(tensorEVD)
+library(ggplot2)
+
+# 1. Simulating A and B matrices
+m = 100; n = 30
+p = 60; q = 200
+A <- matrix(rnorm(m*n), ncol=n)
+B <- matrix(rnorm(p*q), ncol=q)
+
+dm <- c(nrow(A)*nrow(B), ncol(A)*ncol(B))   # dimension of the Kronecker
+
+# 2. Subsetting an small matrix with 20% of rows/columns
+rows1 <- sample(seq(dm[1]), 0.20*dm[1])
+cols1 <- sample(seq(dm[2]), 0.20*dm[2])
+
+# 3. Subsetting a large matrix with 200% of rows/columns
+rows2 <- sample(seq(dm[1]), 2*dm[1], replace=TRUE)
+cols2 <- sample(seq(dm[2]), 2*dm[2], replace=TRUE)
+
+res <- microbenchmark(
+       'Kronecker(A,B,rows,cols)_Small submatrix'  = Kronecker(A,B,rows=rows1,cols=cols1),
+       'Kronecker(A,B)[rows,cols]_Small submatrix' = Kronecker(A,B)[rows1,cols1],
+       'Kronecker(A,B,rows,cols)_Large submatrix'  = Kronecker(A,B,rows=rows2,cols=cols2),
+       'Kronecker(A,B)[rows,cols]_Large submatrix' = Kronecker(A,B)[rows2,cols2],
+     times = 30)
+
+# 4. Making a barplot
+res <- data.frame(expr=res$expr, time=res$time/1E9)
+res <- data.frame(res, do.call(rbind,strsplit(as.character(res$expr),"_")))
+dat <- aggregate(time~X1+X2, data=res, FUN=median)
+dat$X2 <- paste0(dat$X2,": ",ifelse(dat$X2=="Small submatrix",
+                                    paste(length(rows1),"x",length(cols1)),
+                                    paste(length(rows2),"x",length(cols2))))
+
+title <- bquote('Subsetting of Kronecker('*A[.(m)*'x'*.(n)]*', '*B[.(p)*'x'*.(q)]*')'==K[.(m*p)*'x'*.(n*q)])
+
+pp <- ggplot(dat, aes(X1,time,fill=X1)) + theme_bw() +
+      geom_bar(stat="identity", width=0.6) +
+      labs(x=NULL, y="Time (seconds)", title=title, fill=NULL) +
+      facet_wrap(~X2, scales="free") +
+      theme(plot.title=element_text(hjust=0.5),
+            axis.text.x=element_blank(), legend.position="bottom",
+            legend.spacing.x=unit(20,'pt'),
+            legend.text=element_text(margin=margin(l=-18)))
+print(pp)
